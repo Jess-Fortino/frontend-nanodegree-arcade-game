19c6a284e58216009c886363be72abb99f1052f5
JFortino <jess.fortino@gmail.com>
2016-08-08 11:32:27 -0400

    Added movement and rendering -- changed character

--- js/app.js
+++ js/app.js
@@ -13,7 +13,7 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    if(this.x < 505){
+    if(this.x < 705){
       this.x += (this.speed * dt)
     }else{
 //the enemy will reset to the beginning of the x axis with new random speed
@@ -34,19 +34,61 @@ var Player = function(x,y){
   this.sprite = 'images/char-cat-girl.png';
   this.x = x;
   this.y = y;
+//score of the player
+  this.score = 0;
 }
+//holds high score
+var highScore = 0;
+
+//checks if player reaches water and calculates score if gained
+Player.prototype.update = function(){
+  //player hits water
+  if(this.y < 0.5){
+    //reset x & y
+    this.x = 200;
+    this.y = 475;
+    this.score += 10;
+  }
+}
+
+Player.prototype.checkCollisions = function(){
+//checks bug locations for possible collisions -- if hit, player location resets and score is compared to high score
+  for(i = 0; i < 4; i++){
+    if(this.x < allEnemies[i].x + 50 &&
+    this.x + 50 > allEnemies[i].x &&
+    this.y < allEnemies[i].y + 75 &&
+    75 + this.y > allEnemies[i].y) {
+//reset position
+    this.x = 200;
+    this.y = 475;
+//checks high score
+      if(this.score > highScore){
+        highScore = this.score;
+        this.score = 0;
+      }else{
+        this.score = 0;
+      }
+    }
+  }
+};
+
+//renders player onto canvas
+Player.prototype.render = function(){
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  ctx.font = '30px Impact';
+  ctx.fillText(' = x' +this.score, 105, 700);
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 var allEnemies = [];
-//CHANGECHANGECHANGECHANGECHANGECHANGECHANGE
-allEnemies[0] = new Enemy();
-allEnemies[1] = new Enemy();
-allEnemies[2] = new Enemy();
-allEnemies[3] = new Enemy();
+allEnemies[0] = new Enemy(0, 60);
+allEnemies[1] = new Enemy(0, 140);
+allEnemies[2] = new Enemy(0, 230);
+allEnemies[3] = new Enemy(0, 310);
 
-var player = new Player();
+var player = new Player(200, 475);
 
 
 // This listens for key presses and sends the keys to your
@@ -61,3 +103,18 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
+//allows keyboard key movements
+Player.prototype.handleInput = function(key){
+  if(key === "left" && this.x > 0){
+    this.x -=100;
+  }
+  if(key === "right" && this.x < 600){
+    this.x += 100;
+  }
+  if(key === "up" && this.y > 0){
+    this.y -= 85;
+  }
+  if(key === "down" && this.y < 600){
+    this.y += 85;
+  }
+};
