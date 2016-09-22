4ca73a961e35b02733264ebbaa3cc25119b49641
JFortino <jess.fortino@gmail.com>
2016-09-22 11:53:48 -0400

    different styles added

--- js/app.js
+++ js/app.js
@@ -23,15 +23,17 @@ Enemy.prototype.updatePosition = function(dt){
       this.x = -20;
     }
 }
-//checks if the player collides with a bug
+//checks if the player collides with a bug, if the player does collide with a bug, the game resets and the player loses their score.
 Enemy.prototype.checkCollisions = function(){
   if(this.x < player.x + 50 &&
      this.x + 70 > player.x &&
      this.y < player.y + 30 &&
      this.y + 30 > player.y){
-      //player = new Player();
-      //  allEnemies = [new Enemy];
-       console.log("collision");
+       new Player();
+       player.x = 200;
+       player.y = 410;
+       alert("You lost! You had " + score + " points");
+       document.location.reload();
      }
 }
 // Draw the enemy on the screen, required method for game
@@ -47,28 +49,48 @@ var Player = function(x,y){
   this.x = x;
   this.y = y;
 };
-//checks if player reaches water and calculates score if gained
+
+
+//checks if player reaches water and calculates score
 Player.prototype.update = function(){
   //player hits water, resets player position
   if(this.y < 0.5){
-    //reset x & y
+    new Player()
     this.x = 200;
-    this.y = 475;
-    this.score += 1;
+    this.y = 410;
+    console.log("I hit the water!");
+    //alert("You won!");
+    console.log(score++);
+    if(score === 5){
+      alert("You Win with " +score + " points!");
+      document.location.reload();
+    }
   }
 }
+
+//Begin Scoreboard
+var score = 0;
+
+function drawScore() {
+    ctx.font = "16px Arial";
+    ctx.fillStyle = "#000000";
+    ctx.fillText("Score: "+score, 50, 100);
+}
+//drawScore();
+
+
 //allows keyboard key movements
 Player.prototype.handleInput = function(key){
   if(key === "left" && this.x > 0){
     this.x -=100;
   }
-  if(key === "right" && this.x < 606){
+  if(key === "right" && this.x < 400){
     this.x += 100;
   }
   if(key === "up" && this.y > 0){
     this.y -= 85;
   }
-  if(key === "down" && this.y < 600){
+  if(key === "down" && this.y < 400){
     this.y += 85;
   }
 };
@@ -77,34 +99,17 @@ Player.prototype.render = function(){
   ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-//create winning profile
-var score = 0;
-var newLabelScore = document.createElement("label");
-var newTextScore = document.createTextNode("Your Score: " + score);
-newLabelScore.appendChild(newTextScore);
-document.body.appendChild(newLabelScore);
-
-Player.prototype.win = function(){
-  this.y = 400;
-  var i;
-  if((i = 5) || (i > 5)){
-    this.sprite[i] = this.sprite[i + 1];
-  }
-  score++;
-  newTextScore.nodeValue = "Your Score: " + score;
-}
-
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 var allEnemies = [];
-allEnemies[0] = new Enemy(0, 60);
+allEnemies[0] = new Enemy(0, 50);
 allEnemies[1] = new Enemy(0, 140);
 allEnemies[2] = new Enemy(0, 230);
-allEnemies[3] = new Enemy(0, 310);
+// allEnemies[3] = new Enemy(0, 310); //If you want 4 enemies
 
-var player = new Player(200, 475);
+var player = new Player(200, 410);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
